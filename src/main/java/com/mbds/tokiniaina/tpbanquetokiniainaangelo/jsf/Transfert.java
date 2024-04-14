b64c2703ce9f6e05df61661f7544e097d6d8d322
Ramananjoelina Tokiniaina Angelo <ramananjoelinatokiniaina@gmail.com>
2024-04-14 10:06:52 +0300

    Ajout transfert d'argent

--- src/main/java/com/mbds/tokiniaina/tpbanquetokiniainaangelo/jsf/Transfert.java
+++ src/main/java/com/mbds/tokiniaina/tpbanquetokiniainaangelo/jsf/Transfert.java
@@ -6,6 +6,7 @@ package com.mbds.tokiniaina.tpbanquetokiniainaangelo.jsf;
 
 import com.mbds.tokiniaina.tpbanquetokiniainaangelo.entity.CompteBancaire;
 import com.mbds.tokiniaina.tpbanquetokiniainaangelo.service.GestionnaireCompte;
+import com.mbds.tokiniaina.tpbanquetokiniainaangelo.util.Util;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.view.ViewScoped;
@@ -19,7 +20,7 @@ import java.io.Serializable;
  */
 @Named(value = "transfert")
 @ViewScoped
-public class Transfert implements Serializable{
+public class Transfert implements Serializable {
 
     /**
      * Creates a new instance of Transfert
@@ -29,7 +30,7 @@ public class Transfert implements Serializable{
 
     @Inject
     private GestionnaireCompte gestionnaireCompte;
-    
+
     private Long idSource;
     private Long idDestination;
     private int montant;
@@ -57,12 +58,30 @@ public class Transfert implements Serializable{
     public void setMontant(int montant) {
         this.montant = montant;
     }
-    
-    public String transferer(){
-        CompteBancaire source = gestionnaireCompte.findById(idSource);       
+
+    public String transferer() {
+        CompteBancaire source = gestionnaireCompte.findById(idSource);
         CompteBancaire destination = gestionnaireCompte.findById(idDestination);
-        gestionnaireCompte.transferer(source,destination,montant);
+        boolean erreur = false;
+        if (source == null) {
+            // Message d'erreur associé au composant source ; form:source est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+            // dans la page JSF qui lance le transfert.
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            erreur = true;
+        } else {
+            if (source.getSolde() < montant) { // à compléter pour le cas où le solde du compte source est insuffisant...
+                Util.messageErreur("Solde du compte source insuffisante pour le transfert!", "Solde du compte source insuffisante pour le transfert!", "form:source");
+                erreur = true;
+            }
+        }
+        if (erreur) { // en cas d'erreur, rester sur la même page
+            return null;
+        }
+
+        gestionnaireCompte.transferer(source, destination, montant);
+        Util.addFlashInfoMessage("Transfert de la somme de " + montant + " effectué, source: " + source.getNom() + " - " + source.getSolde() + ", destination: " + destination.getNom() + " - " + destination.getSolde());
         return "listeComptes?faces-redirect=true";
     }
-    
+
 }
